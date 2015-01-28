d5f2db43c15d60b0d07b9eac0249a47ad00b65ee
Eric Espinoza <eric@virsix.com>
2015-01-27 19:07:44 -0800

    - Updated EricTestScene - Added networking to target - DarkQuad is the transition plane

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -5,23 +5,26 @@ public class Target : MonoBehaviour {
 
 	private bool m_hit = false;
 	private Animator m_animator;
-
+	private NetworkView myNetworkView;
 
 	void Start () {
 		m_animator = GetComponentInChildren<Animator>();
-
+		myNetworkView = GetComponent<NetworkView>();
 		SetColliders( false );
 	}
 
 	public void Hit() {
-		m_animator.SetTrigger( "Drop" );
-		m_hit = true;
-
-		SetColliders (false);
+		if( Network.isServer ) {
+			m_animator.SetTrigger( "Drop" );
+			myNetworkView.RPC( "SendTrigger", RPCMode.Others, "Drop" );
+			m_hit = true;
+			SetColliders (false);
+		}
 	}
 
 	public void PopUp() {
 		m_animator.SetTrigger( "Rise" );
+		myNetworkView.RPC( "SendTrigger", RPCMode.Others, "Drop" );
 		SetColliders (true);
 	}
 
@@ -35,5 +38,10 @@ public class Target : MonoBehaviour {
 
 	public bool IsHit() {
 		return m_hit;
+	}
+
+	[RPC]
+	void SendTrigger( string name ) {
+		m_animator.SetTrigger( name );
 	}
 }
